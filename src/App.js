488e0ce97ff118060c605ba3de71a54ccb4a386c
HollyLgru <H.grudovich@zollege.com>
2022-10-18 13:29:35 -0500

    17

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 //https://www.youtube.com/watch?v=DQfeB_FKKkc
-
+//17 mins in
 import  React from 'react';
 import './App.css';
 
@@ -8,12 +8,15 @@ import './App.css';
 function App() {
 
 
-
-
   return (
     <div className="App">
       <h1>Woofer App!</h1>
+      {/* header */}
+      {/* user cards */}
+      {/* buttons */}
+
     </div>
-  )
+  );
 }
+
 export default App;
