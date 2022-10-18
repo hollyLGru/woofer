a7f9ad91e337c0a46725dd9381b8f908c364f11e
HollyLgru <H.grudovich@zollege.com>
2022-10-18 13:18:25 -0500

    video

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
-import  {React, useState, useEffect } from 'react';
+
+//https://www.youtube.com/watch?v=DQfeB_FKKkc
+
+import  React from 'react';
 import './App.css';
-import axios from 'axios';
+
 
 function App() {
-  const [dogs , updateDogs] = useState([]);
-  const [name, setName] = useState([]);
-  const [hobby, setHobby] = useState([])
 
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      const dogPicturesAPI = await axios(`https://dog.ceo/api/breeds/image/random`);
-      const randomNameAPI = await axios(`https://random-data-api.com/api/name/random_name`);
-      const activityAPI = await axios(`https://www.boredapi.com/api/activity`);
 
-      updateDogs(dogPicturesAPI.data);
-      setName(randomNameAPI.data);
-      setHobby(activityAPI.data)
-    };
-
-    fetchData()
-
-  }, []);
 
 
   return (
     <div className="App">
-
-          <img src={dogs.message} alt="" style={{width: "300pt"}}></img>
-          <h1>{name.name}</h1>
-          <h2>{hobby.activity}</h2>
-
+      <h1>Woofer App!</h1>
     </div>
-  );
+  )
 }
-//https://www.youtube.com/watch?v=wLGM04oi_wE lets code along to this video to make woofer
 export default App;
---
+++ src/components/Dogs.js
@@ -0,0 +1,39 @@
+import  {React, useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function DogUsers() {
+  const [dogs , updateDogs] = useState([]);
+  const [name, setName] = useState([]);
+  const [hobby, setHobby] = useState([])
+
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const dogPicturesAPI = await axios(`https://dog.ceo/api/breeds/image/random`);
+      const randomNameAPI = await axios(`https://random-data-api.com/api/name/random_name`);
+      const activityAPI = await axios(`https://www.boredapi.com/api/activity`);
+
+      updateDogs(dogPicturesAPI.data);
+      setName(randomNameAPI.data);
+      setHobby(activityAPI.data)
+    };
+
+    fetchData()
+
+  }, []);
+
+
+
+  return (
+    <div className="App">
+
+          <img src={dogs.message} alt="" style={{width: "300pt"}}></img>
+          <h1>{name.name}</h1>
+          <h2>{hobby.activity}</h2>
+
+    </div>
+  );
+}
+
+export default DogUsers;
