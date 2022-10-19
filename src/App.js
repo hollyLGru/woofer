f64d14e39039aff8a3584411902be256969e701e
HollyLgru <H.grudovich@zollege.com>
2022-10-19 13:12:33 -0500

    i material ui

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@
 //17 mins in
 import  React from 'react';
 import './App.css';
+import Header from './components/Header'
 
 
 function App() {
@@ -12,8 +13,11 @@ function App() {
     <div className="App">
       <h1>Woofer App!</h1>
       {/* header */}
+      <Header/>
       {/* user cards */}
       {/* buttons */}
+
+      {/* chat screen */}
 
     </div>
   );
